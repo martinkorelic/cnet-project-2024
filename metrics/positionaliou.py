b34656eeaaec7be3ddaa128764b75f99cf29c479
Gašper <gasper.pistotnik@gmail.com>
2024-05-08 19:22:25 +0200

    mean_reciprocal_rank

--- metrics/positionaliou.py
+++ metrics/positionaliou.py
@@ -17,3 +17,17 @@ def positionaliou(array1, array2):
         return 0
     iou_score = intersection_weighted_count / total_possible_score
     return iou_score
+
+def mean_reciprocal_rank(array1, array2):
+    ranks = []
+    for word1 in array1:
+        for index2, word2 in enumerate(array2):
+            if word1 == word2:
+                ranks.append(1 / (index2 + 1))
+                break
+        else:
+            ranks.append(0)
+    if ranks:
+        return sum(ranks) / len(ranks)
+    else:
+        return 0
